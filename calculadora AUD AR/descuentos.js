b6cd7907d5447c63fe83f2b448d772b32f5ff00a
Rusolea <40546090+Rusolea@users.noreply.github.com>
2023-01-08 23:14:41 -0300

    primer commit

---
+++ calculadora AUD AR/descuentos.js
@@ -0,0 +1,23 @@
+
+  function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const InputDiscount = document.getElementById("InputDiscount");
+  const usdValue = parseInt(InputDiscount.value);
+  
+  let dolarTargeta1 = usdValue + (usdValue * 0.3);
+  let dolarTargeta = dolarTargeta1 + (usdValue * 0.45);
+  /* let dolarTargeta = (dolarTargeta1 * 45 / 100) + dolarTargeta1; */
+  let UsdAu = 0.69
+
+  let valorEnPesos = parseInt(dolarTargeta * UsdAu);
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio en pesos es: $" + valorEnPesos +  ", con un dolar targeta de  $" + dolarTargeta + ("!");
+  
+  console.log(priceValue)
+  console.log(usdValue)
+  console.log(dolarTargeta)
+
+  }
